47735a71f4ff84a9e5f18df48219be4ce1571847
soywiz <soywiz@gmail.com>
2019-10-07 16:04:01 +0200

    Typo

--- kminiorm/src/main/kotlin/com/soywiz/kminiorm/DbTable.kt
+++ kminiorm/src/main/kotlin/com/soywiz/kminiorm/DbTable.kt
@@ -19,7 +19,7 @@ interface DbTable<T : DbTableElement> {
     suspend fun find(skip: Long? = null, limit: Long? = null, query: DbQueryBuilder<T>.() -> DbQuery<T> = { everything }): Iterable<T>
     suspend fun findAll(skip: Long? = null, limit: Long? = null): Iterable<T> = find(skip = skip, limit = limit)
     suspend fun findOne(query: DbQueryBuilder<T>.() -> DbQuery<T> = { everything }): T? = find(query = query, limit = 1).firstOrNull()
-    suspend fun update(set: Partial<T>, increment: Partial<T>?, limit: Long? = null, query: DbQueryBuilder<T>.() -> DbQuery<T>): Long
+    suspend fun update(set: Partial<T>, increment: Partial<T>? = null, limit: Long? = null, query: DbQueryBuilder<T>.() -> DbQuery<T>): Long
     suspend fun <R> transaction(callback: suspend DbTable<T>.() -> R): R
     companion object { }
 }
