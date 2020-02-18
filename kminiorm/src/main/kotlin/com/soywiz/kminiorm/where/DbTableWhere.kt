8b14d7fff6cb687965c462214cce361c4bd244d1
soywiz <soywiz@gmail.com>
2020-02-18 12:50:18 +0100

    Immutable DbTableWhere with cache

--- kminiorm/src/main/kotlin/com/soywiz/kminiorm/where/DbTableWhere.kt
+++ kminiorm/src/main/kotlin/com/soywiz/kminiorm/where/DbTableWhere.kt
@@ -3,30 +3,30 @@ package com.soywiz.kminiorm.where
 import com.soywiz.kminiorm.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import java.util.ArrayList
+import kotlinx.coroutines.sync.*
 import kotlin.reflect.*
 
 data class DbTableWhere<T : DbTableBaseElement>(
-    private val table: DbTable<T>
+    private val table: DbTable<T>,
+    private val fields: List<KProperty1<T, *>>? = null,
+    private val sorted: List<Pair<KProperty1<T, *>, Int>>? = null,
+    private val skip: Long? = null,
+    private val limit: Long? = null,
+    private val andClauses: List<DbQuery<T>> = emptyList()
 ) : Flow<T> {
-    @PublishedApi internal var fields: List<KProperty1<T, *>>? = null
-    @PublishedApi internal var sorted: List<Pair<KProperty1<T, *>, Int>>? = null
-    @PublishedApi internal var skip: Long? = null
-    @PublishedApi internal var limit: Long? = null
-    @PublishedApi internal val andClauses: ArrayList<DbQuery<T>> = arrayListOf()
+    fun fields(vararg fields: KProperty1<T, *>) = this.copy(fields = fields.toList())
+    fun fields(fields: List<KProperty1<T, *>>) = this.copy(fields = fields.toList())
 
-    inline fun fields(vararg fields: KProperty1<T, *>) = this.apply { this.fields = fields.toList() }
-    inline fun fields(fields: List<KProperty1<T, *>>) = this.apply { this.fields = fields }
+    fun sorted(vararg sorted: Pair<KProperty1<T, *>, Int>) = this.copy(sorted = sorted.toList())
+    fun sorted(sorted: List<Pair<KProperty1<T, *>, Int>>) = this.copy(sorted = sorted.toList())
 
-    inline fun sorted(vararg sorted: Pair<KProperty1<T, *>, Int>) = this.apply { this.sorted = sorted.toList() }
-    inline fun sorted(sorted: List<Pair<KProperty1<T, *>, Int>>) = this.apply { this.sorted = sorted }
+    inline fun skip(count: Number) = this.copy(skip = count.toLong())
+    inline fun limit(count: Number) = this.copy(limit = count.toLong())
 
-    inline fun skip(count: Number) = this.apply { this.skip = count.toLong() }
-    inline fun limit(count: Number) = this.apply { this.limit = count.toLong() }
+    @PublishedApi
+    internal val _andClauses get() = andClauses
 
-    inline fun where(query: DbQueryBuilder<T>.() -> DbQuery<T>) = this.apply { this.andClauses += query(
-        DbQueryBuilder.builder()
-    )  }
+    inline fun where(query: DbQueryBuilder<T>.() -> DbQuery<T>) = this.copy(andClauses = _andClauses + query(DbQueryBuilder.builder()))
     inline fun <R> eq(field: KProperty1<T, R>, value: R) = where { field eq value }
     inline fun <R> ne(field: KProperty1<T, R>, value: R) = where { field ne value }
     inline fun <R : Comparable<R>?> gt(field: KProperty1<T, R>, value: R) = where { field gt value }
@@ -36,11 +36,18 @@ data class DbTableWhere<T : DbTableBaseElement>(
     inline fun <R : Comparable<R>> between(field: KProperty1<T, R>, value: ClosedRange<R>) = where { field BETWEEN value }
     inline fun <R : Comparable<R>> IN(field: KProperty1<T, R>, values: List<R>) = where { field IN values }
 
-    suspend fun find(): Flow<T> = table.findFlow(skip = skip, limit = limit, fields = fields, sorted = sorted, query = { AND(andClauses) })
+    private var findCache: List<T>? = null
+    private val lock = Mutex()
+    suspend fun find(): List<T> = lock.withLock {
+        if (findCache == null) {
+            findCache = table.find(skip = skip, limit = limit, fields = fields, sorted = sorted, query = { AND(andClauses) })
+        }
+        findCache!!
+    }
 
     suspend operator fun iterator() = find().toList().iterator()
     @InternalCoroutinesApi
-    override suspend fun collect(collector: FlowCollector<T>) = find().collect(collector)
+    override suspend fun collect(collector: FlowCollector<T>) = find().asFlow().collect(collector)
 }
 
 val <T : DbTableBaseElement> DbTable<T>.where get() = DbTableWhere(this)
