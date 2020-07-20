bbe5d87684b4c30bc905687d39c51588223644b3
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-07-20 12:29:40 +0200

    Update and rename build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+        google()
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs/") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
+    }
+
+    dependencies {
+        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.9.3") // Kotlin 1.3.72: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
+    }
+}
+
+val buildExtraGradleFile = File(rootDir, "build.extra.gradle.kts")
+if (buildExtraGradleFile.exists()) {
+    apply(from = buildExtraGradleFile)
+}
